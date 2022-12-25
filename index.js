81db5e8d6862d9f9387fc894572e2c62268f60a2
Sumon Alam <99078579+smnalam16@users.noreply.github.com>
2022-12-25 11:07:10 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 5684
+const path = require('path')
+
+
+
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+app.post('/comment', (req, res) => {
+    console.log(req.body)  
+    res.json(req.body)
+})
+
+
+app.get('/', (req, res) => {
+     res.sendFile('Post req Recievedl')
+})    
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, './public',  'index.html'))
+
+})
+
+app.get('/Sumonalam', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Sumonalam.html'))
+})
