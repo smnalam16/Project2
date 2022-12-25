bd74ef5cdadee165a1c495556572084cf94c4bf8
Sumon Alam <99078579+smnalam16@users.noreply.github.com>
2022-12-25 11:07:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ res.sendFile(path.join(__dirname, './public',  'index.html'))
 
 })
 
-app.get('/umonalam', (req, res) => {
+app.get('/Sumonalam', (req, res) => {
     res.sendFile(path.join(__dirname, './public',  'Sumonalam.html'))
 })
